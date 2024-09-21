885055c5ddce0952d1c7924c8862d5544bc00d00
kashan16 <mohdkashan21165@gmail.com>
2024-09-22 01:50:20 +0530

    Commit

--- cli_task_tracker/src/main/java/com/example/cli_task_tracker/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cli_task_tracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.cli_task_tracker.entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task,Long>{}
--- cli_task_tracker/src/main/java/com/example/cli_task_tracker/service/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cli_task_tracker.service;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(Long id) {
-        super("Task with ID " + id + " not found.");
-    }
-}
